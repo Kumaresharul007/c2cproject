26766e4401e7b9da1b7bc4522703bc46867afae0
Kumaresharul007 <95173293+Kumaresharul007@users.noreply.github.com>
2022-05-10 21:27:34 +0530

    Create deposits.php

---
+++ deposits.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+  <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet">
+  <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+  <script src="//cdjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet" />
+  <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+  <title>Deposits</title>
+</head>
+<body>
+<div><h1 class="text-center" style="font-weight: 500; color: black;">Deposits</h1></div>
+  <div class="container" id="Card">
+    <div class="card mb-3">
+      <img class="card-img-top" src="corporatedep.jpeg" alt="Card image cap">
+      <div class="card-body">
+        <h3 class="card-title" style="color: rgb(0, 0, 0); font-size: 25px;">Corporate Deposits</h3>
+        <p class="card-text">Deposits from Public Limited Company, Private Limited Company, Corporations, Statutory Board, Local Authorities, Banks, Financial Institutions and other such Institutions as may be decided by the management from time to time.</p>
+        <h4 class="card-text" style="color: rgb(0, 0, 0); font-size: 25px;">Corporate Deposit Features:</h4>
+        <p>FAAA/STABLE rating from CRISIL</p>
+        <p>Prompt Customer Service</p>
+        <p>Electronic Payment Facility for Interest/ Principal payment</p>
+        <p>Quick sanction of Loan against Deposit up to 75% of Deposit Amount</p>
+        <h3>Click here:  <a href="profiledisplay.php"  style="color: #052f89; font-size: 20px; font-weight: 300px;">Apply now</a>
+      </div>
+    </div>
+    
+    <div class="card mb-3">
+      <img class="card-img-top" src="publicdep.jpeg" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title" style="color: rgb(0, 0, 0); font-size: 25px;">Public Deposits</h5>
+        <p class="card-text">Public Deposit will be accepted from Resident Individuals, Non-Resident Individuals, Minors through guardians, HUFs, Partnership Firms, Co-operative Societies, Association of Persons, Proprietary Concern, Trusts and others as decided by management.</p>
+        <h4 class="card-text" style="color: rgb(0, 0, 0); font-size: 25px;">Public Deposit Features :</h4>
+        <p>FAAA/STABLE rating by CRISIL
+        <p>Attractive Card Rates</p>
+        <p>Monthly and Yearly Interest Payment Option under Non- Cumulative Scheme</p>
+        <p>Interest Compounded Annually under Cumulative Scheme</p>
+        <p>Auto Renewal/ Auto Repayment Facility available</p>
+        <p>Electronic Payment Facility for Interest payment and Principal Payment</p>
+        <p>Additional benefit of 0.25% p.a. in interest rate to senior citizens (for deposits upto Rs. 20 Crore on all tenures)</p>
+        <p>Quick sanction of Loan against Deposit up to 75% of Deposit Amount</p>
+        <h3>Click here:  <a href="profiledisplay.php"  style="color: #052f89; font-size: 20px; font-weight: 300px;">Apply now</a>
+      </div>
+    </div>
